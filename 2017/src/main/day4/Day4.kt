46267874bfdedb92d726266c24e18bf401d561d6
Mark Knutson <mark.knutson@code42.com>
2017-12-04 06:16:52 -0600

    day4 part 2

--- 2017/src/main/day4/Day4.kt
+++ 2017/src/main/day4/Day4.kt
@@ -10,6 +10,9 @@ class Day4(private val passPhrasFile: String) {
         }
     }
 
+    /**
+     * valid passwords can not have duplicate phrases
+     */
     private fun isValid(passPhrase: String): Boolean {
         val passPhrases = mutableSetOf<String>()
         passPhrase.split(" ")
@@ -21,10 +24,34 @@ class Day4(private val passPhrasFile: String) {
                 }
         return true
     }
+
+    fun solve2(): Int {
+        File(passPhrasFile).useLines { passwords ->
+            return passwords.filter { p -> isNotAnAnagram(p) }
+                    .count()
+        }
+    }
+
+    /**
+     * valid passwords can not have phrases that are anagrams of eachother
+     */
+    private fun isNotAnAnagram(passPhrase: String): Boolean {
+        val sortedPassPhrases = mutableSetOf<String>()
+        passPhrase.split(" ")
+                .forEach{ passPhrase ->
+                    val sortedPhrase = passPhrase.toCharArray().sortedArray().contentToString()
+                    if (sortedPassPhrases.contains(sortedPhrase)) {
+                        return false
+                    }
+                    sortedPassPhrases.add(sortedPhrase)
+                }
+        return true
+    }
 }
 
 fun main(args: Array<String>) {
     val passPhrases = "src/main/day4/input2.txt"
     val solver = Day4(passPhrases)
     println("Valid phrases: ${solver.solve1()}")
+    println("Valid phrases2: ${solver.solve2()}")
 }
