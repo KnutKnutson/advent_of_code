d3725411cf8bf3f68b4a17e5dda11f7e1803c67c
Mark Knutson <knutson.mc@gmail.com>
2017-12-07 06:01:37 -0600

    new files for day 7

---
+++ 2017/src/main/day7/Day7.kt
@@ -0,0 +1,56 @@
+package main.day6
+
+import java.io.File
+
+class Day7(private val memoryBanks: IntArray)  {
+
+    private val banksLength = memoryBanks.size
+
+    fun solve1(): Int {
+        val seen = mutableListOf(memoryBanks.contentToString())
+        val banks = memoryBanks.copyOf()
+        var redistributionCount = 0
+        while (true) {
+            redistribute(banks)
+            redistributionCount++
+            if (seen.contains(banks.contentToString())) {
+                return redistributionCount
+            }
+            seen.add(banks.contentToString())
+        }
+    }
+
+    private fun redistribute(banks: IntArray) {
+        val max = banks.max()!!
+        var index = banks.indexOf(max)
+        banks[index] = 0
+        (1..max).forEach {
+            index = (index + 1) % banksLength
+            banks[index]++
+        }
+    }
+
+    fun solve2(): Int {
+        val seen = mutableListOf(memoryBanks.contentToString())
+        val banks = memoryBanks.copyOf()
+        while (true) {
+            redistribute(banks)
+            val value = banks.contentToString()
+            if (seen.contains(value)) {
+                return seen.size - seen.indexOf(value)
+            }
+            seen.add(value)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val memoryBanksFile = "src/main/day6/input.txt"
+    File(memoryBanksFile).useLines { lines ->
+        lines.forEach { line ->
+            val solver = Day6(line.split("\\s+".toRegex()).map{i->Integer.parseInt(i)}.toIntArray())
+            println("Redistributions: ${solver.solve1()}")
+            println("Cycles in loop: ${solver.solve2()}")
+        }
+    }
+}
