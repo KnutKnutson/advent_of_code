fb374d029405bd3e860f90bdcbe5c3ae62b165c3
Mark Knutson <knutson.mc@gmail.com>
2017-12-08 06:47:22 -0600

    day7

--- 2017/src/main/day7/Day7.kt
+++ 2017/src/main/day7/Day7.kt
@@ -1,56 +1,88 @@
-package main.day6
+package main.day7
 
 import java.io.File
 
-class Day7(private val memoryBanks: IntArray)  {
+open class Program(var name: String) {
 
-    private val banksLength = memoryBanks.size
+    var weight = 0
+    var parent: Program? = null
+    var children = mutableListOf<Program>()
+}
 
-    fun solve1(): Int {
-        val seen = mutableListOf(memoryBanks.contentToString())
-        val banks = memoryBanks.copyOf()
-        var redistributionCount = 0
-        while (true) {
-            redistribute(banks)
-            redistributionCount++
-            if (seen.contains(banks.contentToString())) {
-                return redistributionCount
+class Day7(file: String)  {
+
+    private val programs = mutableMapOf<String, Program>()
+
+    init {
+        File(file).useLines { lines ->
+            lines.forEach { line ->
+                parseLine(line)
             }
-            seen.add(banks.contentToString())
         }
     }
 
-    private fun redistribute(banks: IntArray) {
-        val max = banks.max()!!
-        var index = banks.indexOf(max)
-        banks[index] = 0
-        (1..max).forEach {
-            index = (index + 1) % banksLength
-            banks[index]++
+    fun solve1(): String {
+        return programs.values.first { p -> p.parent == null }.name
+    }
+
+    private fun parseLine(programLine: String) {
+        val program = programLine.split("(", ")", "->", ",")
+                .map{ s -> s.trim()}
+                .filter{ s -> !s.isBlank()}
+                .toTypedArray()
+
+        val p = programs.getOrPut(program[0]) { Program(program[0]) }
+        p.weight = program[1].toInt()
+
+        (2 until program.size).forEach { i ->
+            val childName = program[i]
+            val child = programs.getOrPut(childName) { Program(childName) }
+            child.parent = p
+            p.children.add(child)
         }
     }
+
+    var idealWeight = 0
 
     fun solve2(): Int {
-        val seen = mutableListOf(memoryBanks.contentToString())
-        val banks = memoryBanks.copyOf()
-        while (true) {
-            redistribute(banks)
-            val value = banks.contentToString()
-            if (seen.contains(value)) {
-                return seen.size - seen.indexOf(value)
-            }
-            seen.add(value)
-        }
+        val root = programs.values.first { p -> p.parent == null }
+        sumChildWeight(root)
+        return idealWeight
     }
+
+    private fun sumChildWeight(program: Program):Int {
+        val childWeights = mutableMapOf<String, Int>()
+        program.children.forEach { c ->
+            childWeights.put(c.name, sumChildWeight(c))
+        }
+
+        // Check if we found the first uneven weights
+        if (idealWeight == 0 && !childWeightsAreEqual(childWeights.values.toList())) {
+            idealWeight = getIdealChildWeight(childWeights.map { e -> Pair(e.key, e.value)})
+        }
+
+        return program.weight + childWeights.values.sum()
+    }
+
+    private fun childWeightsAreEqual(childWeights: List<Int>): Boolean {
+        if (childWeights.isEmpty()) {
+            return true
+        }
+        return childWeights.first() == childWeights.sum() / childWeights.size
+    }
+
+    private fun getIdealChildWeight(childWeights: List<Pair<String, Int>>): Int {
+        val normalChildWeight = childWeights.map {c -> c.second}.sorted()[1]
+        val oddChild = childWeights.find { i -> i.second != normalChildWeight }
+        val childProgram = programs[oddChild?.first]
+        return childProgram!!.weight + (normalChildWeight - oddChild!!.second)
+    }
+
 }
 
 fun main(args: Array<String>) {
-    val memoryBanksFile = "src/main/day6/input.txt"
-    File(memoryBanksFile).useLines { lines ->
-        lines.forEach { line ->
-            val solver = Day6(line.split("\\s+".toRegex()).map{i->Integer.parseInt(i)}.toIntArray())
-            println("Redistributions: ${solver.solve1()}")
-            println("Cycles in loop: ${solver.solve2()}")
-        }
-    }
+    val memoryBanksFile = "src/main/day7/input.txt"
+    val solver = Day7(memoryBanksFile)
+    println("Base Program: ${solver.solve1()}")
+    println("Proper weight: ${solver.solve2()}")
 }
